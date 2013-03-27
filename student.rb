e38403244c1afea43acbed73585d42d79b5625bf
Constantin <mitides.constantin@gmail.com>
2013-03-27 15:54:21 -0400

    added a self.create_person method with case for student and instructor

--- student.rb
+++ student.rb
@@ -2,20 +2,15 @@ require 'rubygems'
 require 'yaml'
 
 class Person
-  attr_accessor :name
-  attr_accessor :email
-  attr_accessor :github_user
-  attr_accessor :twitter
-  attr_accessor :fun_fact
-  attr_accessor :favorite_bar
+  attr_accessor :name, :email, :github_user, :twitter, :fun_fact, :favorite_bar, :favorite_color
 end
 
 class Student < Person
-  attr_accessor :reason_for_joining
+  attr_accessor :reason_for_joining, :bday, :pets
 end
 
 class Instructor < Person
-  attr_accessor :type
+  attr_accessor :type, :like_students
 end
 
 @directory = ""
@@ -23,39 +18,43 @@ puts "Student Directory, v0.0.1 by Dan Garland"
 print "Enter Student or Instructor, q to save and quit: "
 
 while ((input = gets.strip.chomp) != 'q') do
-
   person = nil
   case input
-  when 'Student' 
-    print "Why are you taking this course? "
-    Student.reason_for_joining = gets.strip.chomp
-    # person = Student.new
-    # print "What is your name? "
-    # person.name = gets.strip.chomp
-    # print "What is your email? "
-    # person.email = gets.strip.chomp
-    # print "whats your favorite bar? "
-    # person.favorite_bar = gets.strip.chomp
 
-  when 'Student' || 'Instructor'
+  when 'Student' 
+    person = Student.new
     print "What is your name? "
     person.name = gets.strip.chomp
     print "What is your email? "
     person.email = gets.strip.chomp
-    print "What sort of instructor are you? "
-    person.type = gets.strip.chomp
-    
+    print "Why are you taking this course? "
+    person.reason_for_joining = gets.strip.chomp
+    print "whats your favorite bar? "
+    person.favorite_bar = gets.strip.chomp
+    print "whats your favorite color? "
+    person.favorite_color = gets.strip.chomp
+    print "when is your birthday? "
+    person.bday = gets.strip.chomp
+    print "do you have any pets? "
 
     
   when 'Instructor'
+    print "What is your name? "
+    person.name = gets.strip.chomp
+    print "What is your email? "
+    person.email = gets.strip.chomp
+    print "Why are you taking this course? "
+    person.reason_for_joining = gets.strip.chomp
+    print "whats your favorite bar? "
+    person.favorite_bar = gets.strip.chomp
+    print "whats your favorite color? "
     person = Instructor.new
-    # print "What is your name? "
-    # person.name = gets.strip.chomp
-    # print "What is your email? "
-    # person.email = gets.strip.chomp
-    # print "What sort of instructor are you? "
-    # person.type = gets.strip.chomp
+    print "do you like your students?"
+    person.like_students = gets.strip.chomp
+
   end
+
+
   
   # Append this to our yaml file
   @directory += person.to_yaml
